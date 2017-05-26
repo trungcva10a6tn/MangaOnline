97fe7cd32bbe009d2057e0b156b9b7720ece2aa8
trungcva10a6tn <trung@NAL01.local>
2017-05-26 08:27:16 +0700

    trung

--- js/MangaJs.js
+++ js/MangaJs.js
@@ -1,3 +1,14 @@
 /**
  * Created by trung on 04/05/2017.
  */
+function auto_load(id,url_manga) {
+$.ajax({
+    success: function(data) {
+        $("#"+id).html(data);
+    },
+    url: url_manga,
+    error: function() {
+        alert("bạn bị mất mạng");
+    }
+});
+}
